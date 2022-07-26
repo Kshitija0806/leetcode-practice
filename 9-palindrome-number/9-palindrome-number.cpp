c46527a047796a3d0f20d86a130d9a0ad0d36155
Kshitija0806 <102468513+Kshitija0806@users.noreply.github.com>
2022-07-26 22:31:08 +0530

    Time: 27 ms (36.50%), Space: 6 MB (31.51%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -2,27 +2,41 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         
+//         if(x < 0){
+//             return false;
+//         }
+//         vector<int> vec;
+//         stack<int> st;
+//         int n=x;
+        
+//         while(n!=0){
+//             int rem=n%10;
+//             st.push(rem);
+//             vec.push_back(rem);
+//             n=n/10;
+//         }
+//         // int j=st.top();
+//         for(int i=0;i<vec.size();i++){
+//             int pop_ele=st.top();
+//             st.pop();
+//             if(pop_ele!=vec[i]){
+//                 return false;
+//             }
+//         }
+//         return true;
+        
         if(x < 0){
             return false;
         }
-        vector<int> vec;
-        stack<int> st;
+        
         int n=x;
+        unsigned long int sum=0;
         
         while(n!=0){
-            int rem=n%10;
-            st.push(rem);
-            vec.push_back(rem);
+            sum=sum*10+n%10;
             n=n/10;
         }
-        // int j=st.top();
-        for(int i=0;i<vec.size();i++){
-            int pop_ele=st.top();
-            st.pop();
-            if(pop_ele!=vec[i]){
-                return false;
-            }
-        }
-        return true;
+        
+        return (sum==x);
     }
 };
