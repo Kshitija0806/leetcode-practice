db705b29c62897ded8d46f5b7d8d7a92a069c2ba
Kshitija0806 <102468513+Kshitija0806@users.noreply.github.com>
2022-07-28 22:12:51 +0530

    Time: 0 ms (100.00%), Space: 6 MB (47.68%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -1,12 +1,22 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
+//         int res=0;
+//         while(n>0){
+//             if(n%2!=0){
+//                 res++;
+//             }
+//             n=n/2;
+//         }
+        
+//         return res;
+        
+        //Brian Kerningam's algo
+        
         int res=0;
-        while(n>0){
-            if(n%2!=0){
-                res++;
-            }
-            n=n/2;
+        while(n > 0){
+            n=(n&(n-1));
+            res++;
         }
         
         return res;
