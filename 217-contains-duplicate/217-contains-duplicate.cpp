f9bb8af3e0b3ce36227f9a56065f8dbf728756bb
Kshitija0806 <102468513+Kshitija0806@users.noreply.github.com>
2022-07-30 13:47:42 +0530

    Time: 149 ms (51.38%), Space: 46.6 MB (79.42%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -15,16 +15,28 @@ public:
         //Efficient solution
         //For this problem we will use set
         
-        unordered_set<int> res;
         
-        for(int i=0;i<nums.size();i++){
-            if(res.find(nums[i])==res.end()){  //means present
-                res.insert(nums[i]);
-            }else{
+        //Better solution
+        
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]==nums[i+1]){
                 return true;
             }
         }
         
         return false;
+        
+//         unordered_set<int> res;
+        
+//         for(int i=0;i<nums.size();i++){
+//             if(res.find(nums[i])==res.end()){  //means present
+//                 res.insert(nums[i]);
+//             }else{
+//                 return true;
+//             }
+//         }
+        
+//         return false;
     }
 };
