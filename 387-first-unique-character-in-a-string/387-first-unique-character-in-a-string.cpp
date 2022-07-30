1480319b8ee336f05841e11cc41cf773ed4c868e
Kshitija0806 <102468513+Kshitija0806@users.noreply.github.com>
2022-07-30 14:54:56 +0530

    Time: 61 ms (46.38%), Space: 10.6 MB (71.15%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
@@ -1,17 +1,37 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        int count[26]={0};
+        
+        //Naive solution
         
         for(int i=0;i<s.length();i++){
-            count[s[i]-'a']++;
-        }
-        
-        for(int i=0;i<s.length();i++){
-            if(count[s[i]-'a']==1){
+            bool flag=true;
+            for(int j=0;j<s.length();j++){
+                if(i!=j && s[i]==s[j]){  //means not distinct
+                    flag=false;
+                    break;
+                }
+            }
+            if(flag==true){
                 return i;
             }
+            
         }
-        return -1;
+            return -1;
+        
+        
+        //Efficient solution
+//         int count[26]={0};
+        
+//         for(int i=0;i<s.length();i++){
+//             count[s[i]-'a']++;
+//         }
+        
+//         for(int i=0;i<s.length();i++){
+//             if(count[s[i]-'a']==1){
+//                 return i;
+//             }
+//         }
+//         return -1;
     }
 };
