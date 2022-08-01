333e6e934aac9623979bfc6f4f42a214c172c9bb
Kshitija0806 <102468513+Kshitija0806@users.noreply.github.com>
2022-08-01 22:44:41 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (26.72%) - LeetHub

--- 202-happy-number/202-happy-number.cpp
+++ 202-happy-number/202-happy-number.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     bool isHappy(int n) {
-        if((n==1) || (n==7)){
-            return true;
+        
+        unordered_set<int> mp;
+        
+        while(n!=1){
+            if(mp.find(n)==mp.end()){
+                mp.insert(n);
+            }else 
+                return false;
+            
+            int temp=n;
+            int ans=0;
+            while(temp!=0){
+                ans+=(temp%10)*(temp%10);
+                temp=temp/10;
+            }
+            n=ans;
+            
         }
-        
-        else if((n < 10)){
-            return false;
-        }
-        
-        int temp=n;
-        int ans=0;
-        while(temp!=0){
-            ans+=((temp%10)*(temp%10));
-            temp=temp/10;
-        }
-        
-        return (isHappy(ans));
-        
-        
+        return true; 
     }
 };
