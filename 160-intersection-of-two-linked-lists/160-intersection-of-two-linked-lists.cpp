dd2cf0b407964a8a58f4359b5bcc524d43fae8f2
Kshitija0806 <102468513+Kshitija0806@users.noreply.github.com>
2022-08-04 22:30:36 +0530

    Time: 77 ms (43.51%), Space: 14.6 MB (72.74%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -9,18 +9,49 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        unordered_set<ListNode*> st;
-        for(ListNode* curr=headA;curr!=NULL;curr=curr->next){
-            st.insert(curr);
+        //Now there is another way to use two pointers.
+        //First we will find length of list1 ans list2 and difference of l1 and l2.
+        
+        ListNode* curr1=headA, *curr2=headB;
+        
+        int l1=0, l2=0;
+        
+        while(curr1!=NULL){
+            l1++;
+            curr1=curr1->next;
+        }
+        
+        while(curr2!=NULL){
+            l2++;
+            curr2=curr2->next;
         }
         
         
-        
-        for(ListNode *curr1=headB;curr1!=NULL;curr1=curr1->next){
-            if(st.find(curr1)!=st.end()){
-                return curr1;
+        curr1=headA;
+         curr2=headB;
+        int d;
+        if(l1 > l2){
+            d=l1-l2;
+    
+            while(d--){
+                curr1=curr1->next;
+            }
+        }else{
+            d=l2-l1;
+            while(d--){
+                curr2=curr2->next;
             }
         }
+        
+        while(curr1!=NULL && curr2!=NULL){
+            if(curr1==curr2){
+                return curr1;
+            }
+            
+            curr1=curr1->next;
+            curr2=curr2->next;
+        }
+        
         
         return NULL;
     }
