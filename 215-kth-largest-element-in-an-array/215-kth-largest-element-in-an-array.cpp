819fc4b88a696b34c2ecc388ac78ded71015fc57
Kshitija0806 <102468513+Kshitija0806@users.noreply.github.com>
2022-08-27 19:32:55 +0530

    Time: 115 ms (31.46%), Space: 47.4 MB (9.41%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -14,5 +14,23 @@ public:
             }
         }
         return minpq.top();
+        
+//         //For kth largest element in an array we will have to take min heap.
+//         //In c++, by default the priority queue is max heap so we have to convert it into min heap
+//         //by adding a negative sign to it will inserting
+        
+//         priority_queue<int> pq;
+        
+//         for(int i=0;i<k;i++){
+//             pq.push(-1*nums[i]);   ///push first k elements in queue
+//         }
+        
+//         for(int i=k;i<nums.size();i++){
+//             if(pq.top()<nums[i]){  //replace upcoming element is greater we have to replace the peak
+//                 pq.pop();
+//                 pq.push(-1*nums[i]);
+//             }
+//         }
+//         return (-1*pq.top());
     }
 };
