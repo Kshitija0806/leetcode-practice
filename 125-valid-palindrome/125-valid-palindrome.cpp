92a3600f3c7642a001ca46787d008a52040ecfbe
Kshitija0806 <102468513+Kshitija0806@users.noreply.github.com>
2022-08-31 14:29:34 +0530

    Time: 6 ms (75.35%), Space: 7.3 MB (77.04%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -21,22 +21,24 @@ public:
 //             j--;
 //         }
 //         return true;
-        // int i=0,j=s.length()-1;
-        // while(i<j){
-        //     while(isalpha(s[i])==false && i<j){i++;}
-        //     while(isalpha(s[i])==false && i<j){j--;}
-        //     if(toupper(s[i])!=toupper(s[j]))return false;
-        //     i++;
-        //     j--;
-        // }
-        // return true;
         
-        for (int i = 0, j = s.size() - 1; i < j; i++, j--) { // Move 2 pointers from each end until they collide
-        while (isalnum(s[i]) == false && i < j) i++; // Increment left pointer if not alphanumeric
-        while (isalnum(s[j]) == false && i < j) j--; // Decrement right pointer if no alphanumeric
-        if (toupper(s[i]) != toupper(s[j])) return false; // Exit and return error if not match
-    }
+        
+        int i=0,j=s.length()-1;
+        while(i<j){
+            while(isalnum(s[i])==false && i<j){i++;}
+            while(isalnum(s[j])==false && i<j){j--;}
+            if(toupper(s[i])!=toupper(s[j]))return false;
+            i++;
+            j--;
+        }
+        return true;
+        
+//         for (int i = 0, j = s.size() - 1; i < j; i++, j--) { // Move 2 pointers from each end until they collide
+//         while (isalnum(s[i]) == false && i < j) i++; // Increment left pointer if not alphanumeric
+//         while (isalnum(s[j]) == false && i < j) j--; // Decrement right pointer if no alphanumeric
+//         if (toupper(s[i]) != toupper(s[j])) return false; // Exit and return error if not match
+//     }
     
-    return true;
+//     return true;
     }
 };
