ed72a3386d8852695df03d241a3525aedd34cb8f
Kshitija0806 <102468513+Kshitija0806@users.noreply.github.com>
2022-09-16 09:12:46 +0530

    Time: 20 ms (54.36%), Space: 18.3 MB (92.90%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
@@ -5,17 +5,29 @@ public:
         
         //Using set
         
-        set<int> st;  //here in the set we have to maintain order of elements
+//         set<int> st;  //here in the set we have to maintain order of elements
         
-        for(int i=0;i<nums.size();i++){
-            st.insert(nums[i]);
+//         for(int i=0;i<nums.size();i++){
+//             st.insert(nums[i]);
+//         }
+        
+//         int k=st.size();
+//         int j=0;
+//         for(auto x: st){
+//             nums[j++]=x;
+//         }
+//         return k;
+        
+        //Efficient approach: Two pointer approach
+        
+        int i=0;
+        for(int j=1;j<nums.size();j++){
+            if(nums[i]!=nums[j]){  //when we get a different element fro the current
+                i++; //first move i forward
+                nums[i]=nums[j];   //replace arr[i] with arr[j]
+            }
         }
         
-        int k=st.size();
-        int j=0;
-        for(auto x: st){
-            nums[j++]=x;
-        }
-        return k;
-    }
+        return i+1;   //at last return i+1
+   }
 };
