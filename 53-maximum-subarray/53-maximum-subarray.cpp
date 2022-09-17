be4ba9358405022713e7dd8ac10a009fc43a4894
Kshitija0806 <102468513+Kshitija0806@users.noreply.github.com>
2022-09-17 15:58:39 +0530

    Time: 120 ms (92.41%), Space: 67.8 MB (53.60%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,31 +1,25 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        // int max_sum=INT_MIN;
-        // for(int i=0;i<nums.size();i++){
-        // int sum=0;
-        //     for(int j=i;j<nums.size();j++){
-        //         sum+=nums[j];
-        //         max_sum=max(max_sum,sum);
-        //     }
-        // }
-        // return max_sum;
+        //Naive solution: Form every subarray and find maximum sum and length
+        //Time : O(n^2)
         
-        int max_sum=nums[0];
-        //Here we are not taking max_sum=INT_MIN because in the question it is asked to have at least one
-        //number in the subarray
-        int sum=0; //initialize sum=0
+        //Efficient approach is to use Kadane's algorithm
         
-        for(int i=0;i<nums.size();i++){
-            sum+=nums[i];  //we will take sum of numbers in a variable sum
-            if(sum > max_sum){  //if sum is greater than max_sum we will update max_sum to sum
-                max_sum=sum;
-            }
-            
-            if(sum < 0){ //But if sum is less than zero than we will not take it forward and make it zero
-                sum=0; //As we have to find maximum sum taking a negative value forward is not a good idea
+        int sum=0,max_sum=INT_MIN;
+        //The basic idea of Kadane's algorithm is that he don't have to take a negative sum forward
+        //because it decreses the max_sum
+        //So then the sum becomes negative we make sum=0
+        //And everytime we compare sum and max_sum
+        
+        for(auto i:nums){
+            sum+=i;
+            max_sum=max(max_sum,sum);
+            if(sum<0){
+                sum=0;
             }
         }
+        
         return max_sum;
     }
 };
