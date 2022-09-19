a6e6e8041d0160ebf4c6f679e73f60cc92fa8d7a
Kshitija0806 <102468513+Kshitija0806@users.noreply.github.com>
2022-09-19 07:48:43 +0530

    Time: 378 ms (44.72%), Space: 51.1 MB (32.89%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -3,26 +3,57 @@ public:
     int longestConsecutive(vector<int>& nums) {
         //Naive solution using sorting
         
-       if(nums.size() == 0 ){
-            return 0;
+//        if(nums.size() == 0 ){
+//             return 0;
+//         }
+        
+//         sort(nums.begin(),nums.end());
+        
+//         int ans = 1;
+//         int prev = nums[0];
+//         int cur = 1;
+        
+//         for(int i = 1;i < nums.size();i++){
+//             if(nums[i] == prev+1){
+//                 cur++;
+//             }
+//             else if(nums[i] != prev){
+//                 cur = 1;
+//             }
+//             prev = nums[i];
+//             ans = max(ans, cur);
+//         }
+//         return ans;
+        
+        //Efficient solution: To use set
+        //We will use set and not unoredered_set because the order matters
+        
+        set<int> st;
+        
+        for(auto x:nums){
+            st.insert(x);
         }
         
-        sort(nums.begin(),nums.end());
         
-        int ans = 1;
-        int prev = nums[0];
-        int cur = 1;
+        int max_len=0;
+        int curr_len=0;
+        int curr_num;
         
-        for(int i = 1;i < nums.size();i++){
-            if(nums[i] == prev+1){
-                cur++;
+        for(auto x:st){
+            if(st.find(x-1)==st.end()){
+                curr_len=1;
+                curr_num=x;
             }
-            else if(nums[i] != prev){
-                cur = 1;
+            
+            while(st.find(curr_num+1)!=st.end()){
+                curr_len++;
+                curr_num++;
             }
-            prev = nums[i];
-            ans = max(ans, cur);
-        }
-        return ans;
+            
+            max_len=max(max_len,curr_len);
+                
     }
+        return max_len;
+    }
+    
 };
