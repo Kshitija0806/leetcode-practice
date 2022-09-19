a15408abdc21bf7be1f960ed9ee30558266e038e
Kshitija0806 <102468513+Kshitija0806@users.noreply.github.com>
2022-09-19 08:22:51 +0530

    Time: 41 ms (6.04%), Space: 13.3 MB (54.28%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,43 +1,62 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        //Brute force approach
-        //Linearly traverse the matrix and if encountered with a zero replace that row and column with -1
-        //excpt for zero because it may modify other values also
-        //Time : O(n*m)*(n+m)
+       //Brute force approach: Assuming that the matrix contains non-negative elements
         
-        //Better solution:
-        //We can maintain one dummy column array and a dummy row array
-        //We will linearly traverse the array and if we get zero at any index we will set its dummy column as 
-        // zero and dummy row as zero.
-        //Then we will again traverse the matrix and whereever the dummy has zero values we will set that row and //column as zero
+        //1. Traverse the matrix and if we find any zero then change all elements in its row and column to -1
+        //except the zeroes. not changing zero as it may affect other rows and columns
         
-        //Efficient approach:
-        //Instead of using two dummy arrays we can use it in the matrix only .
+        //2. Again traverse the matrix and set all -1 to zeroes
         
-        int col0=true; //this variable is to see that we don't modify first row and column wrong
-        int rows=matrix.size(),cols=matrix[0].size();
+        int rows=matrix.size();
+        int cols=matrix[0].size();
+        
+//         for(int i=0;i<rows;i++){
+//             for(int j=0;j<cols;j++){
+//                 if(matrix[i][j]==0){
+//                     for(int k=0;k<cols;k++){
+//                         if(matrix[i][k]!=0)matrix[i][k]=-1;
+//                     }
+                    
+//                     for(int k=0;k<rows;k++){
+//                         if(matrix[k][j]!=0)matrix[k][j]=-1;
+//                     }
+//                 }
+//             }
+//         }
+        
+        
+//         for(int i=0;i<rows;i++){
+//             for(int j=0;j<cols;j++){
+//                 if(matrix[i][j]==-1){
+//                     matrix[i][j]=0;
+//                 }
+//             }
+//         }
+        
+//         Time complexity: O((N*M)*(N+M)
+//                            N*m - for traversing through matrix
+//                            N+M - for traversing individual rows and columns
+        
+        //Better solution: To use dummmy arrays
+        
+       vector < int > dummy1(rows,-1), dummy2(cols,-1);
         
         for(int i=0;i<rows;i++){
-            if(matrix[i][0]==0)col0=0; //we will check is first column element in every row is zero or not.
-            //if it is zero set cols=false
-            
-            for(int j=1;j<cols;j++){ //traversing will start from second column always
+            for(int j=0;j<cols;j++){
                 if(matrix[i][j]==0){
-                    matrix[i][0]=matrix[0][j]=0;  //we will set both row and column as zero
+                    dummy1[i]=0;
+                    dummy2[j]=0;
                 }
             }
         }
         
-        //Now again we will traverse the matrix.But this time we will traverse from back
-        for(int i=rows-1;i>=0;i--){
-            for(int j=cols-1;j>=1;j--){
-                if(matrix[i][0]==0 || matrix[0][j]==0){
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(dummy1[i]==0 || dummy2[j]==0){
                     matrix[i][j]=0;
                 }
             }
-            if(col0==0)matrix[i][0]=0;
         }
-        
     }
 };
