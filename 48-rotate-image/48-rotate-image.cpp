60507e8f2bf6b8d0550f7248edd0dc07a07dd388
Kshitija0806 <102468513+Kshitija0806@users.noreply.github.com>
2022-09-19 08:53:00 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (33.34%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,31 +1,20 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        //Naive solution:
-        //Use an auxiliary matrix and make first row as last column, second row as second last column and so on.
+        //Naive solution : Use a dummy matrix. Traverse the matrix and put first row in last column of matrix
+        //Put second row in second last column of matrix
         
-        //Efficient solution:
-        //1.Take transpose of the matrix.
-        //2.then reverse individual rows one by one
-        
-        //For finding transpose of a matrix we use the upper triangular matrix trick
-        //In a matrix will finding a transpose the diagonal elements never change. Only the element 
-        //above and below it are swapped
-        
-        for(int i=0;i<matrix.size();i++){
-            for(int j=i+1;j<matrix.size();j++){
-                swap(matrix[i][j],matrix[j][i]);
-            }
-        }
-        
-        //Now reversing individual rows
-        for(int i=0;i<matrix.size();i++){
-            int low=0,high=matrix[0].size()-1;
-            while(low<high){
-                swap(matrix[i][low],matrix[i][high]);
-                low++;
-                high--;
-                }
-        }
+        int n = matrix.size();
+  vector < vector < int >> rotated(n, vector < int > (n, 0));
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      rotated[j][n - i - 1] = matrix[i][j];
+    }
+  }
+  for(int i=0;i<matrix.size();i++){
+      for(int j=0;j<matrix.size();j++){
+          matrix[i][j]=rotated[i][j];
+      }
+  }
     }
 };
