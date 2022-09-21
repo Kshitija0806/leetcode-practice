4e24fc25c7002c667a186bc78281a7160f269cda
Kshitija0806 <102468513+Kshitija0806@users.noreply.github.com>
2022-09-21 16:44:14 +0530

    Time: 10 ms (77.20%), Space: 13.6 MB (84.49%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -3,19 +3,45 @@ public:
     vector<int> searchRange(vector<int>& nums, int target) {
      //Naive solution: Traversing the array
         
+//         int fOcc=-1,lastOcc=-1;
+//         vector<int> ans;
+        
+//         for(int i=0;i<nums.size();i++){
+//             if(nums[i]==target){
+//                 if(fOcc==-1){
+//                     fOcc=i;
+//                     lastOcc=i;
+//                 }else{
+//                     lastOcc=i;
+//                 }
+//             }
+//         }
+//         ans.push_back(fOcc);
+//         ans.push_back(lastOcc);
+//         return ans;
+        
+        //Also we know that the array is sorted. So for last occurence we will start from the back
+        
         int fOcc=-1,lastOcc=-1;
         vector<int> ans;
+        int n=nums.size();
         
-        for(int i=0;i<nums.size();i++){
+        for(int i=n-1;i>=0;i--){
             if(nums[i]==target){
-                if(fOcc==-1){
+                lastOcc=i;
+                break;
+            }
+        }
+        
+        if(lastOcc!=-1){
+            for(int i=0;i<n;i++){
+                if(nums[i]==target){
                     fOcc=i;
-                    lastOcc=i;
-                }else{
-                    lastOcc=i;
+                    break;
                 }
             }
         }
+        
         ans.push_back(fOcc);
         ans.push_back(lastOcc);
         return ans;
