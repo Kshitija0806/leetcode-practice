b9c59e93822e4c5387316c75db432e52e00eb0a3
Kshitija0806 <102468513+Kshitija0806@users.noreply.github.com>
2022-09-26 10:53:09 +0530

    Time: 40 ms (5.63%), Space: 7.6 MB (38.41%) - LeetHub

--- 237-delete-node-in-a-linked-list/237-delete-node-in-a-linked-list.cpp
+++ 237-delete-node-in-a-linked-list/237-delete-node-in-a-linked-list.cpp
@@ -9,8 +9,10 @@
 class Solution {
 public:
     void deleteNode(ListNode* node) {
-        node->val=node->next->val;
+       //If we have to delete a node from middle of linked-list and we are not given head
+        //then apply the concept of putting the next value in the node to be deleted and delete the next node
         ListNode* temp=node->next;
+        node->val=node->next->val;
         node->next=node->next->next;
         delete temp;
         
