82c9fa36b37877aee807e4ab57392179abe169db
Kshitija0806 <102468513+Kshitija0806@users.noreply.github.com>
2022-11-03 19:34:15 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (41.64%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -11,15 +11,37 @@
  */
 class Solution {
 public:
-    vector<int> result;
+    // vector<int> result;
     vector<int> preorderTraversal(TreeNode* root) {
-        vector<int> v;
-        if(root==NULL)return v;
+        //This is the recursive way of doing preorder
+//         vector<int> v;
+//         if(root==NULL)return v;
         
-            result.push_back(root->val);
-            preorderTraversal(root->left);
-            preorderTraversal(root->right);
+//             result.push_back(root->val);
+//             preorderTraversal(root->left);
+//             preorderTraversal(root->right);
         
-        return result;
+//         return result;
+        
+        //For the iterative way we will use stack data structure
+        vector<int> preorder;
+        if(root==NULL)return preorder;
+        
+        //Now initialize a stack
+        stack<TreeNode*> st;
+        //At the first step we have to push the root in the stack
+        st.push(root);
+        while(!st.empty()){
+            root=st.top();
+            st.pop();
+            preorder.push_back(root->val);
+            if(root->right!=NULL){
+                st.push(root->right);
+            }
+            if(root->left!=NULL){
+                st.push(root->left);
+            }
+        }
+        return preorder;
     }
 };
